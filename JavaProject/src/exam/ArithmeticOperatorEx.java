471be8f36578c697e99573897f4f8d046d34f988
Choi Jung Hwan <choijh5779@gmail.com>
2026-07-21 14:16:18 +0900

    Initial Upload

--- JavaProject/src/exam/ArithmeticOperatorEx.java
+++ JavaProject/src/exam/ArithmeticOperatorEx.java
@@ -1,12 +1,13 @@
 package exam;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class ArithmeticOperatorEx {
 	public static void main(String[] args) {
 		 Scanner sc = new Scanner(System.in);
-		 int kor, eng, math;
-		 float total, average;
+		 int kor, eng, math, total;
+		 float average;
 		 
 		 System.out.print("국어 점수 입력 : ");
 		 kor = sc.nextInt();
@@ -20,7 +21,12 @@ public class ArithmeticOperatorEx {
 		 total = kor + eng + math;
 		 average = total / 3;
 		 
+		 // 소수점 이하 자리수 두자리로 조정
+		 DecimalFormat df = new DecimalFormat("0.00");
+		 
 		 System.out.println("총점 : " + total);
-		 System.out.println("평균 : " + average);
+		 System.out.println("평균 : " + df.format(average));
+		 
+		 sc.close();
 	}
 }
