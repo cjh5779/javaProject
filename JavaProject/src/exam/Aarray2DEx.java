41ade7771dded4717443bd259cb17e52af6ba1f0
Choi Jung Hwan <choijh5779@gmail.com>
2026-07-23 18:21:04 +0900

    exam upload

--- JavaProject/src/exam/Aarray2DEx.java
+++ JavaProject/src/exam/Aarray2DEx.java
@@ -1,12 +1,28 @@
 package exam;
 
+import java.util.Scanner;
+
 public class Aarray2DEx {
 	public static void main(String[] args) {
-		String[][] word = {{"chair", "의자"}, {"computer", "컴퓨터"}, {"integer", "정수"}};
+		Scanner sc = new Scanner(System.in);
+		String[][] words = {{"chair", "의자"}, {"computer", "컴퓨터"}, {"integer", "정수"}};
+		String answer;
 		
-		for(int i=0; i<word.length; i++) {
+		for(int i=0; i<words.length; i++) {
+			System.out.printf("Q%d. %s의 뜻은? : ", i+1, words[i][0]);
+			answer = sc.nextLine();
+			
+			if(answer.equals(words[i][1])) {
+				System.out.println("정답입니다.");
+			} else {
+				System.out.printf("틀렸습니다. 정답은 %s입니다.\n", words[i][1]);
+			}
+			
+			System.out.println();
 			
 		}
+		
+		sc.close();
 		
 	}
 
