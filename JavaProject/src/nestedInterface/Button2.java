b4cfb9f6c66737e750987d50721197ec4951e9f9
Choi Jung Hwan <choijh5779@gmail.com>
2026-07-31 09:18:18 +0900

    7/30 upload

--- JavaProject/src/nestedInterface/Button2.java
+++ JavaProject/src/nestedInterface/Button2.java
@@ -1,22 +1,20 @@
 package nestedInterface;
 
 public class Button2 {
-    
-    public interface OnClickListener {
-        void onClick();
-    }
+	OnClickListener listener; 
+	
+	//구현 객체 따로 만들지 않고
+	//클래스 내부에서 직접 익명 객체 대입해서 이벤트 처리하는 경우가 더 많다
+	public void setListener(OnClickListener listener) {
+		this.listener = listener;
+	}
 
-    OnClickListener listener; 
-    
-    // 구현 객체 따로 만들지 않고
-    // 클래스 내부에서 직접 익명 객체 대입해서 이벤트 처리하는 경우가 더 많다
-    public void setListener(OnClickListener listener) {
-        this.listener = listener;
-    }
-    
-    public void touch() {
-        if (listener != null) {
-            listener.onClick();
-        }
-    }
+	//버튼 이벤트가 발생하면 호출하는 메서드
+	void touch() {
+		listener.onClick();
+	}
+
+	interface OnClickListener{
+		void onClick();
+	}
 }
--- JavaProject/src/nestedclass/access/local/OutterMain.java
+++ JavaProject/src/nestedclass/access/local/OutterMain.java
@@ -1,11 +1,15 @@
 package nestedclass.access.local;
 
-public class OutterMain {
-	public static void main(String[] args) {
-		// 인스턴스 멤버 클래스 객체 생성 시 바깥 클래스 객체 생성 후 가능
-		Outter outter = new Outter();
-		Outter.Nestrd nested = outter.new Nested();
-		nested.print();
+public class Outter2 {
+	public void method2(final int arg1, int arg2) { //매개변수
+		final int localVariable1 = 1; //로컬변수
+		arg2 = 200; //변경되면 final이 아니라고 해석
+		
+		class Inner{ //로컬 클래스
+			public void method() {
+				//처리 진행 시 매개변수나 로컬변수 사용시 final 인것만 사용하자
+				int result = arg1 + localVariable1 + arg2; //final이 아니므로 사용 불가능
+			}
+		}
 	}
-
 }
