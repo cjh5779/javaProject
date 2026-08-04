8a6b4eac0401aa936c1e691ae2adff2876d47aa4
Choi Jung Hwan <choijh5779@gmail.com>
2026-08-04 09:22:10 +0900

    8/4 upload

--- JavaProject/src/generic/gen/Box.java
+++ JavaProject/src/generic/gen/Box.java
@@ -1,7 +1,7 @@
 package generic.gen;
 
-// generic 타입 클래스 구현 - T는 아직 결정되지 않은 타입
-public class Box <T>{
+//generic 타입 클래스 구현 - T는 아직 결정되지 않은 타입
+public class Box<T> {
 	private T product;
 	
 	public void set(T product) {
